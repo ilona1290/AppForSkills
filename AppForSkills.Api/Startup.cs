6726c27ebd9c5a922d9b4bd87c3593a4c6785e7e
Ilona Lapka <ilona2000123@wp.pl>
2021-04-10 15:20:29 +0200

    Added Dependency Injection to Infrastructure project

--- AppForSkills.Api/Startup.cs
+++ AppForSkills.Api/Startup.cs
@@ -1,3 +1,4 @@
+using AppForSkills.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,6 +34,7 @@ namespace AppForSkills.Api
             {
                 builder.WithOrigins("https://localhost:44390");
             }));
+            services.AddInfrastructure(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
