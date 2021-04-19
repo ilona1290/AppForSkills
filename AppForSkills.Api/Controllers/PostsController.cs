ef7902654924f46884cf344aa1aa186001088a90
Ilona Lapka <ilona2000123@wp.pl>
2021-04-19 17:18:58 +0200

    Method GetPhotoOrVideo has been added

---
+++ AppForSkills.Api/Controllers/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Api.Controllers
+{
+    [Route("api/posts/{id}")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        /// <summary>
+        /// Get photo or video of user skill
+        /// </summary>
+        [Route("photo-video")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> GetPhotoOrVideo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
