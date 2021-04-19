478d4fa30787fcc29522038a5a1db82083eee7d8
Ilona Lapka <ilona2000123@wp.pl>
2021-04-19 18:52:08 +0200

    Method AddCommentToUserSkill has been added

--- AppForSkills.Api/Controllers/PostsController.cs
+++ AppForSkills.Api/Controllers/PostsController.cs
@@ -12,37 +12,78 @@ namespace AppForSkills.Api.Controllers
     public class PostsController : ControllerBase
     {
         /// <summary>
-        /// Returns photo or video with general information of user skill.
+        /// Returns all user skills.
         /// </summary>
-        /// <param name="id">Post id</param>
-        [Route("{id}/photo-video")]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public string GetPhotoOrVideo(int id)
+        public string GetAllSkills()
         {
             return "value";
+        }
+        /// <summary>
+        /// Returns photo or video with general information of user skill.
+        /// </summary>
+        /// <param name="id">Post id</param>
+        [Route("{id}")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public string GetSkillWithGeneralInformation(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Adds skill.
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public void AddSkill()
+        {
+            
         }
 
         /// <summary>
         /// Returns comments, which user skill receive.
         /// </summary>
         /// <param name="id">Post id</param>
-        [Route("comments")]
+        [Route("{id}/comments")]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public List<string> GetCommentsToUserPost(int id)
+        public List<string> GetCommentsToUserSkill(int id)
         {
             return new List<string> { "value1", "value2" };
         }
 
+        /// <summary>
+        /// Adds comment to user skill.
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <param name="value">Post id</param>
+        [Route("{id}/comments")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
 
+        public void AddCommentToUserSkill(int id, [FromBody] string value)
+        {
+        }
     }
 }
