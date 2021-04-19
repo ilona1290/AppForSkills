893587d860dfbeb4e1b26cdfc921e5ea0f9a3242
Ilona Lapka <ilona2000123@wp.pl>
2021-04-19 18:33:19 +0200

    Method GetDescriptionAboutUserPost has been removed, names of methods has been changed and method GetAllSkills has been added

--- AppForSkills.Api/Controllers/PostsController.cs
+++ AppForSkills.Api/Controllers/PostsController.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace AppForSkills.Api.Controllers
 {
-    [Route("api/posts/{id}")]
+    [Route("api/posts")]
     [ApiController]
     public class PostsController : ControllerBase
     {
         /// <summary>
-        /// Return photo or video of user skill.
+        /// Returns photo or video with general information of user skill.
         /// </summary>
         /// <param name="id">Post id</param>
-        [Route("photo-video")]
+        [Route("{id}/photo-video")]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -28,25 +28,9 @@ namespace AppForSkills.Api.Controllers
         }
 
         /// <summary>
-        /// Return description about photo or video, which has been posted by user.
+        /// Returns comments, which user skill receive.
         /// </summary>
-        /// <param name="id">Id of post, which description we want to get.</param>
-        [Route("description")]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-
-        public string GetDescriptionAboutUserPost(int id)
-        {
-            return "value";
-        }
-
-        /// <summary>
-        /// Return comments to photo or video, which has been posted by user.
-        /// </summary>
-        /// <param name="id">Id of post, which comments we want to get.</param>
+        /// <param name="id">Post id</param>
         [Route("comments")]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -58,5 +42,7 @@ namespace AppForSkills.Api.Controllers
         {
             return new List<string> { "value1", "value2" };
         }
+
+
     }
 }
