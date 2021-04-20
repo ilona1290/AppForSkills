cfb96847b544c4fef3208f40d3db89ba5cd96ffc
Ilona Lapka <ilona2000123@wp.pl>
2021-04-20 16:25:21 +0200

    Methods, which adds, edits and deletes comments to user skill has been added

--- AppForSkills.Api/Controllers/UserController.cs
+++ AppForSkills.Api/Controllers/UserController.cs
@@ -57,6 +57,66 @@ namespace AppForSkills.Api.Controllers
         }
 
         /// <summary>
+        /// Returns comments of selected user posts. 
+        /// </summary>
+        [Route("skills/{id}/comments")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public string GetCommentsFromUserSkill(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Adds comment of selected user posts. 
+        /// </summary>
+        [Route("skills/{id}/comments")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public void CreateCommentToUserSkill(int id, string comment)
+        {
+            
+        }
+
+        /// <summary>
+        /// Edits comment of selected user posts. 
+        /// </summary>
+        [Route("skills/{id}/comments")]
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public void EditCommentToUserSkill(int id, int idComment, string comment)
+        {
+            
+        }
+
+        /// <summary>
+        /// Deletes comment of selected user posts. 
+        /// </summary>
+        [Route("skills/{id}/comments")]
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public void DeleteCommentFromUserSkill(int id, int idComment)
+        {
+
+        }
+
+        /// <summary>
         /// Returns all ratings, which user gave to other users. 
         /// </summary>
         [Route("ratings")]
@@ -70,5 +130,7 @@ namespace AppForSkills.Api.Controllers
         {
             return "value";
         }
+
+
     }
 }
