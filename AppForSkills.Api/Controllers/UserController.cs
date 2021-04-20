dc0fc5234407ef4a84940bd8f9b8a3add12c8844
Ilona Lapka <ilona2000123@wp.pl>
2021-04-20 16:14:55 +0200

    Method, which returns comments to user skill has been added

--- AppForSkills.Api/Controllers/UserController.cs
+++ AppForSkills.Api/Controllers/UserController.cs
@@ -14,13 +14,14 @@ namespace AppForSkills.Api.Controllers
         /// <summary>
         /// Returns general information about user.
         /// </summary>
+        /// <param name="username"></param>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public string GetAllInformation()
+        public string GetAllInformation(string username)
         {
             return "value";
         }
@@ -35,7 +36,22 @@ namespace AppForSkills.Api.Controllers
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public string GetAllUserSkills()
+        public string GetAllUserSkills(string username)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Returns selected user posts. 
+        /// </summary>
+        [Route("skills/{id}")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public string GetUserSkill(int id)
         {
             return "value";
         }
@@ -50,7 +66,7 @@ namespace AppForSkills.Api.Controllers
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public string GetAllRatingsWhichUserGave()
+        public string GetAllRatingsWhichUserGave(string username)
         {
             return "value";
         }
