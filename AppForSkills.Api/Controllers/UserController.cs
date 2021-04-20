bdaedf4cda3c544148c029e6f3e1c880c4ceeac2
Ilona Lapka <ilona2000123@wp.pl>
2021-04-20 15:10:44 +0200

    Method, which returns all information about user has been added

---
+++ AppForSkills.Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Api.Controllers
+{
+    [Route("api/user/{username}")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// Returns general information about user.
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public string GetAllInformation()
+        {
+            return "value";
+        }
+    }
+}
