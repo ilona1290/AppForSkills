ed8ca49c078f2e322f2cfb8e399a2af0df0b282b
Ilona Lapka <ilona2000123@wp.pl>
2021-04-20 16:52:31 +0200

    Method, which returns user achievements has been added

--- AppForSkills.Api/Controllers/UserController.cs
+++ AppForSkills.Api/Controllers/UserController.cs
@@ -176,6 +176,20 @@ namespace AppForSkills.Api.Controllers
             return "value";
         }
 
+        /// <summary>
+        /// Returns all user achievements. 
+        /// </summary>
+        [Route("achievements")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public string GetUserAchievements(string username)
+        {
+            return "value";
+        }
 
     }
 }
