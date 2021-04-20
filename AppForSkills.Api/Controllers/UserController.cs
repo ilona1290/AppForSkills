166a4584e8290af31654a1efac6b90cf5e22599a
Ilona Lapka <ilona2000123@wp.pl>
2021-04-20 15:27:23 +0200

    Method, which returns all user ratings, which user gave has been added

--- AppForSkills.Api/Controllers/UserController.cs
+++ AppForSkills.Api/Controllers/UserController.cs
@@ -39,5 +39,20 @@ namespace AppForSkills.Api.Controllers
         {
             return "value";
         }
+
+        /// <summary>
+        /// Returns all ratings, which user gave to other users. 
+        /// </summary>
+        [Route("ratings")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public string GetAllRatingsWhichUserGave()
+        {
+            return "value";
+        }
     }
 }
