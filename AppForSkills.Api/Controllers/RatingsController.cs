0479a5bfcd01b57fdfd43967926d7285df31a621
Ilona Lapka <ilona2000123@wp.pl>
2021-04-22 13:37:41 +0200

    Sorting users in one method has been added to RatingsController

--- AppForSkills.Api/Controllers/RatingsController.cs
+++ AppForSkills.Api/Controllers/RatingsController.cs
@@ -12,78 +12,19 @@ namespace AppForSkills.Api.Controllers
     public class RatingsController : ControllerBase
     {
         /// <summary>
-        /// Returns user ranking sorted by the number of inserted skill posts .
+        /// Returns user ranking sorted by chosen element.
         /// </summary>
-        [Route("most-posts-of-skills")]
+        /// <param name="element">Choose one of the elements: skillPosts, bestRatings, numberOfGivingRatings, numberOfGivingComments, postsInDiscussions </param>
+        [Route("users/orderBy={element}:desc")]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public string GetRankingOfTheMostPostingSkillsUsers()
+        public string GetRankingAboutUser(string element)
         {
             return "value";
-        }
-
-        /// <summary>
-        /// Returns user ranking sorted by best average ratings.
-        /// </summary>
-        [Route("best-ratings")]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-
-        public string GetRankingOfTheBestAverageRatings()
-        {
-            return "value";
-        }
-
-        /// <summary>
-        /// Returns users ranking sorted by the number of comments to skill posts.
-        /// </summary>
-        [Route("most-comments-to-skills")]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-
-        public string GetRankingOfTheMostCommentingUsers()
-        {
-            return "value";
-        }
-
-        /// <summary>
-        /// Returns users ranking sorted by the number of ratings to skill posts.
-        /// </summary>
-        [Route("most-ratings-to-skills")]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-
-        public string GetRankingOfTheMostEvaluativeUsers()
-        {
-            return "value";
-        }
-
-        /// <summary>
-        /// Returns users ranking sorted by the most posts in discussions.
-        /// </summary>
-        [Route("most-posts-in-discussions")]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-
-        public string GetRankingOfTheMostActiveUsersInDiscussions()
-        {
-            return "value";
-        }
+        } 
     }
 }
