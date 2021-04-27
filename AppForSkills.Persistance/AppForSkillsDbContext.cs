cea4dc6d3eed3c9e3e44ed746490cb0230b098a1
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 18:40:40 +0200

    DbContext has been initially created

---
+++ AppForSkills.Persistance/AppForSkillsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Persistance
+{
+    public class AppForSkillsDbContext : DbContext
+    {
+        public AppForSkillsDbContext(DbContextOptions<AppForSkillsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
