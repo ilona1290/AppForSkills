aad3070747f854946355ab5b6f9fa3842763ec51
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 23:13:26 +0200

    Created Discussion Entity

--- AppForSkills.Domain/Entities/Discussion.cs
+++ AppForSkills.Domain/Entities/Discussion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppForSkills.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace AppForSkills.Domain.Entities
 {
-    public class Discussion
+    public class Discussion : AuditableEntity
     {
+        public string FirstPost { get; set; }
+        public ICollection<PostInDiscussion> PostInDiscussions { get; set; }
+        public int NumberOfPosts { get; set; }
+        public int NumberOfLikes { get; set; }
+        public ICollection<LikeToPost> Likes { get; set; }
+
     }
 }
