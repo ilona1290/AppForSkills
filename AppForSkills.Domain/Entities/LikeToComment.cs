ff52d5534639acfa077c301ac61ce21ba6b2c2e0
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 20:45:19 +0200

    Relocated Entities connected with Skill Posts

--- AppForSkills.Domain/Entities/LikeToComment.cs
+++ AppForSkills.Domain/Entities/LikeToComment.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using AppForSkills.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppForSkills.Domain.Common
+namespace AppForSkills.Domain.Entities
 {
     public class LikeToComment : AuditableEntity
     {
