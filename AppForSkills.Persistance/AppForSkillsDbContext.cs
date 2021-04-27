4f16b4a68bfaf6e10cceb06c769fd13e020055e9
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 19:25:03 +0200

    Overrided SaveChangesAsync method

--- AppForSkills.Persistance/AppForSkillsDbContext.cs
+++ AppForSkills.Persistance/AppForSkillsDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AppForSkills.Domain.Common;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AppForSkills.Persistance
@@ -12,6 +14,39 @@ namespace AppForSkills.Persistance
         public AppForSkillsDbContext(DbContextOptions<AppForSkillsDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = string.Empty;
+                        entry.Entity.Created = DateTime.Now;
+                        entry.Entity.StatusId = 1;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.Modified = DateTime.Now;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.Modified = DateTime.Now;
+                        entry.Entity.Inactivated = DateTime.Now;
+                        entry.Entity.InactivatedBy = string.Empty;
+                        entry.Entity.StatusId = 0;
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
