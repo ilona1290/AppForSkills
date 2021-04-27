6add0a7fcf50a8d54f09d0eb195ccc8bf62ee40e
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 23:31:47 +0200

    Created Reported Post Entity

---
+++ AppForSkills.Domain/Entities/RepostedPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Domain.Entities
+{
+    public class RepostedPost
+    {
+        public int Id { get; set; }
+        public int ReportedPostId { get; set; }
+        public string UserWhoReportedPost { get; set; }
+    }
+}
