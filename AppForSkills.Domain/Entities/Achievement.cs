7c3cd790c3b37349ed1acd4f0024444b2bbc9a42
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 22:59:28 +0200

    Created Achievement Entity

---
+++ AppForSkills.Domain/Entities/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Domain.Entities
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<UserInformation> UsersWithThisAchivement { get; set; }
+    }
+}
---
+++ AppForSkills.Domain/Entities/UserInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Domain.Entities
+{
+    public class UserInformation
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime RecentLoginDate { get; set; }
+        public ICollection<SkillPost> UserSkills { get; set; }
+        public ICollection<Comment> UserComments { get; set; }
+        public ICollection<Rating> GavedRatings { get; set; }
+        public ICollection<Achievement> Achievements { get; set; }
+        public ICollection<Discussion> Discussions { get; set; }
+    }
+}
