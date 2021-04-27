4ef87fb0bb4e690bbdc670e4de31ac5f6012fa5e
Ilona Lapka <ilona2000123@wp.pl>
2021-04-27 23:27:46 +0200

    Created Like to Discussion and Like to Post Entity

---
+++ AppForSkills.Domain/Entities/LikeToPost.cs
@@ -0,0 +1,14 @@
+﻿using AppForSkills.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Domain.Entities
+{
+    public class LikeToPost : AuditableEntity
+    {
+        public int PostInDiscussionId { get; set; }
+    }
+}
