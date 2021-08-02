57c3c8e985ef59b55318c2f02d4a3f9806c12235
Ilona Lapka <ilona2000123@wp.pl>
2021-08-02 20:10:02 +0200

    Added Validators to the rest Commands in Discussion

---
+++ AppForSkills.Application/Discussions/Commands/DeleteDiscussion/DeleteDiscussionValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Application.Discussions.Commands.DeleteDiscussion
+{
+    public class DeleteDiscussionValidator : AbstractValidator<DeleteDiscussionCommand>
+    {
+        public DeleteDiscussionValidator()
+        {
+            RuleFor(d => d.Id).GreaterThan(0);
+        }
+    }
+}
---
+++ AppForSkills.Application/Discussions/Commands/DeletePost/DeletePostValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Application.Discussions.Commands.DeletePost
+{
+    public class DeletePostValidator : AbstractValidator<DeletePostCommand>
+    {
+        public DeletePostValidator()
+        {
+            RuleFor(p => p.PostId).GreaterThan(0);
+        }
+    }
+}
