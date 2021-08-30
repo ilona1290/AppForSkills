a73970d27e269d267ab86da7f9ccb09281551a03
Ilona Lapka <ilona2000123@wp.pl>
2021-08-30 15:45:46 +0200

    Added database mock

---
+++ Application.UnitTests/Common/AppForSkillsDbContextFactory.cs
@@ -0,0 +1,61 @@
+﻿using AppForSkills.Application.Common.Interfaces;
+using AppForSkills.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Common
+{
+    public class AppForSkillsDbContextFactory
+    {
+        public static Mock<AppForSkillsDbContext> Create()
+        {
+            var dateTime = new DateTime(2000, 1, 1);
+            var dateTimeMock = new Mock<IDateTime>();
+            dateTimeMock.Setup(m => m.Now).Returns(dateTime);
+
+            var currentUserMock = new Mock<ICurrentUserService>();
+            currentUserMock.Setup(m => m.Username).Returns("user");
+            currentUserMock.Setup(m => m.IsAuthenticated).Returns(true);
+
+            var options = new DbContextOptionsBuilder<AppForSkillsDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var mock = new Mock<AppForSkillsDbContext>(options, dateTimeMock.Object, currentUserMock.Object) { CallBase = true };
+
+            var context = mock.Object;
+
+            context.Database.EnsureCreated();
+
+            var discussion = new AppForSkills.Domain.Entities.Discussion()
+            {
+                Id = 3,
+                FirstPost = "Jakie sporty uprawiacie?",
+                StatusId = 1
+            };
+            context.Discussions.Add(discussion);
+
+            var postInDiscussion = new AppForSkills.Domain.Entities.PostInDiscussion()
+            {
+                Id = 7,
+                DiscussionId = 3,
+                PostText = "Piłkę nożną",
+                StatusId = 1
+            };
+            context.PostsInDiscussion.Add(postInDiscussion);
+
+            context.SaveChanges();
+            return mock;
+        }
+
+        public static void Destroy(AppForSkillsDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
--- Application.UnitTests/Common/CommandTestBase.cs
+++ Application.UnitTests/Common/CommandTestBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppForSkills.Persistance;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace Application.UnitTests.Common
 {
-    public class CommandTestBase
+    public class CommandTestBase : IDisposable
     {
+        protected readonly AppForSkillsDbContext _context;
+        protected readonly Mock<AppForSkillsDbContext> _contextMock;
+
+        public CommandTestBase()
+        {
+            _contextMock = AppForSkillsDbContextFactory.Create();
+            _context = _contextMock.Object;
+        }
+
+        public void Dispose()
+        {
+            AppForSkillsDbContextFactory.Destroy(_context);
+        }
     }
 }
