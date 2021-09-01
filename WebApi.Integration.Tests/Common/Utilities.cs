5f0c57d4476ca52f903e2ed401f527f727840517
Ilona Lapka <ilona2000123@wp.pl>
2021-09-01 18:01:02 +0200

    Configured Environment To Integration Tests and added first Integration Test

---
+++ WebApi.Integration.Tests/Common/Utilities.cs
@@ -0,0 +1,52 @@
+﻿using AppForSkills.Persistance;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApi.Integration.Tests.Common
+{
+    public class Utilities
+    {
+        public static async Task<T> GetResponseContent<T>(HttpResponseMessage response)
+        {
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<T>(stringResponse);
+
+            return result;
+        }
+        public static void InitiliazeDbForTests(AppForSkillsDbContext context)
+        {
+            var discussion = new AppForSkills.Domain.Entities.Discussion()
+            {
+                Id = 3,
+                FirstPost = "Jakie sporty uprawiacie?",
+                StatusId = 1
+            };
+            context.Discussions.Add(discussion);
+
+            var postInDiscussion = new AppForSkills.Domain.Entities.PostInDiscussion()
+            {
+                Id = 7,
+                DiscussionId = 3,
+                PostText = "Piłkę nożną",
+                StatusId = 1
+            };
+            context.PostsInDiscussion.Add(postInDiscussion);
+
+            var user = new AppForSkills.Domain.Entities.User()
+            {
+                Username = "user",
+                RegistrationDate = new DateTime(1999, 12, 29),
+                RecentLoginDate = new DateTime(2000, 1, 1),
+                StatusId = 1
+            };
+            context.Users.Add(user);
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ WebApi.Integration.Tests/Controllers/Discussions/GetDiscussion_Tests.cs
@@ -0,0 +1,36 @@
+﻿using AppForSkills.Api;
+using AppForSkills.Application.Discussions.Queries.GetDiscussion;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Integration.Tests.Common;
+using Xunit;
+
+namespace WebApi.Integration.Tests.Controllers.Discussions
+{
+    public class GetDiscussion_Tests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public GetDiscussion_Tests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GivenDiscussionId_ReturnsDiscussionDetail()
+        {
+            var client = await _factory.GetAuthenticatedClientAsync();
+
+            string id = "2";
+            var response = await client.GetAsync($"/api/discussions/{id}/posts");
+            response.EnsureSuccessStatusCode();
+
+            var vm = await Utilities.GetResponseContent<DiscussionVm>(response);
+            vm.ShouldNotBeNull();
+            vm.FirstPost.ShouldBe("Jaki kraj chcielibyście odwiedzić?");
+        }
+    }
+}
