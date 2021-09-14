b3455547c5e2779868d9e584a61417405788ec79
Ilona Lapka <ilona2000123@wp.pl>
2021-09-14 15:51:22 +0200

    Added Integration Tests to Queries of SkillPost and Delete SkillPost

---
+++ WebApi.Integration.Tests/Controllers/SkillPosts/GetAllSkillPosts_Tests.cs
@@ -0,0 +1,37 @@
+﻿using AppForSkills.Api;
+using AppForSkills.Application.SkillPosts.Queries.GetSkillPosts;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Integration.Tests.Common;
+using Xunit;
+
+namespace WebApi.Integration.Tests.Controllers.SkillPosts
+{
+    public class GetAllSkillPosts_Tests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public GetAllSkillPosts_Tests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsAllSkillPosts()
+        {
+            var client = await _factory.GetAuthenticatedClientAsync();
+
+
+            var response = await client.GetAsync($"/api/posts");
+            response.EnsureSuccessStatusCode();
+
+            var vm = await Utilities.GetResponseContent<SkillPostsVm>(response);
+
+            vm.SkillPosts.ShouldBeOfType<List<SkillPostDto>>();
+            vm.SkillPosts.Count.ShouldBe(2);
+        }
+    }
+}
