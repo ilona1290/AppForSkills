3916f2d52dffcd2676de87e3959c88c9191cae83
Ilona Lapka <ilona2000123@wp.pl>
2022-02-04 16:44:55 +0100

    Added main page with all posts and initial design for SkillPost detail.

---
+++ AppForSkills/Client/Models/ListSkillPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Client.Models
+{
+    public class ListSkillPost
+    {
+        public ICollection<SkillPostDto> SkillPosts { get; set; }
+    }
+}
---
+++ AppForSkills/Client/Models/SkillPostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Client.Models
+{
+    public class SkillPostDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string AddressOfPhotoOrVideo { get; set; }
+        public string Title { get; set; }
+        public int Views { get; set; }
+        public int Comment { get; set; }
+        public int Rating { get; set; }
+    }
+}
