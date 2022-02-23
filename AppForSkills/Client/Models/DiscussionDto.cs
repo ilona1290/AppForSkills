841d27d9761b888581fe92620ba0884364e16835
Ilona Lapka <ilona2000123@wp.pl>
2022-02-23 22:37:55 +0100

    Added showing discussions

---
+++ AppForSkills/Client/Models/DiscussionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Client.Models
+{
+    public class DiscussionDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstPost { get; set; }
+        public int Posts { get; set; }
+        public int Likes { get; set; }
+        public int Users { get; set; }
+    }
+}
---
+++ AppForSkills/Client/Models/ListDiscussions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Client.Models
+{
+    public class ListDiscussions
+    {
+        public ICollection<DiscussionDto> Discussions { get; set; }
+    }
+}
