99bde683a2af96a95ad0b4089c95fd2fa5b0fb42
Ilona Lapka <ilona2000123@wp.pl>
2022-03-01 00:02:57 +0100

    Added showing User Skills and created component to showing Skill Posts

--- AppForSkills.Application/Users/Queries/GetUserInformation/UserInformationVm.cs
+++ AppForSkills.Application/Users/Queries/GetUserInformation/UserInformationVm.cs
@@ -20,11 +20,12 @@ namespace AppForSkills.Application.Users.Queries.GetUserInformation
         public void Mapping(Profile profile)
         {
             profile.CreateMap<User, UserInformationVm>()
+                .ForMember(s => s.RegistrationDate, map => map.MapFrom(src => src.RegistrationDate))
+                .ForMember(s => s.RecentLoginDate, map => map.MapFrom(src => src.RecentLoginDate))
                 .ForMember(s => s.UserSkills, map => map.MapFrom(src => src.UserSkills.Count))
                 .ForMember(s => s.UserComments, map => map.MapFrom(src => src.UserComments.Count))
                 .ForMember(s => s.GavedRatings, map => map.MapFrom(src => src.GavedRatings.Count))
-                .ForMember(s => s.Achievements, map => map.MapFrom(src => src.Achievements.Count))
-                .ForMember(s => s.Discussions, map => map.MapFrom(src => src.Discussions.Count));
+                .ForAllOtherMembers(s => s.Ignore());
         }
     }
 }
---
+++ AppForSkills/Client/Models/UserInformationVm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Client.Models
+{
+    public class UserInformationVm
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime RecentLoginDate { get; set; }
+        public int UserSkills { get; set; }
+        public int UserComments { get; set; }
+        public int GavedRatings { get; set; }
+        public int Achievements { get; set; }
+        public int Discussions { get; set; }
+    }
+}
