4ad0056484224d379c269890e090bde5ad48b49e
Ilona Lapka <ilona2000123@wp.pl>
2022-04-10 00:53:23 +0200

    Corrected appearance of showing discusions and user panel

---
+++ AppForSkills.Application/Users/Commands/UpdateAvatar/UpdateAvatarCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Application.Users.Commands.UpdateAvatar
+{
+    public class UpdateAvatarCommand : IRequest
+    {
+        public string Username { get; set; }
+        public string Avatar { get; set; }
+    }
+}
---
+++ AppForSkills/Client/Models/UpdateAvatar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppForSkills.Client.Models
+{
+    public class UpdateAvatar
+    {
+        public string Username { get; set; }
+        public string Avatar { get; set; }
+    }
+}
